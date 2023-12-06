6c4342bba92b2a0902e7dba5511eb4dec5273476
“inakuuun” <bkg.inamoto@gmail.com>
2023-12-07 08:06:53 +0900

    ★NavMenuボタン処理追加対応 https://github.com/inakuuun/IksInfoShare/issues/11

---
+++ src/IksInfoShareApp/IksNativeClient/Common/Common/ScreenDef.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IksNativeClient.Common.Common
+{
+    /// <summary>
+    /// 画面部定義クラス
+    /// </summary>
+    public static class ScreenDef
+    {
+        /// <summary>画面ID：友達一覧画面</summary>
+        public const short SCREEN_ID_FRIENDS = 0;
+        /// <summary>画面ID：チャットルーム画面</summary>
+        public const short SCREEN_ID_CHATROOM = 1;
+        /// <summary>画面ID：友達追加画面</summary>
+        public const short SCREEN_ID_FRIEND_ADD = 2;
+    }
+}
