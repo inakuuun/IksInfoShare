73e2e0d9b76904f28e8ba068d0756f30a3e9249e
“inakuuun” <bkg.inamoto@gmail.com>
2023-12-09 16:31:06 +0900

    リファクタリング

--- src/IksInfoShareApp/IksNativeClient/Common/Common/ScreenDef.cs
+++ src/IksInfoShareApp/IksNativeClient/Common/Common/ScreenDef.cs
@@ -11,11 +11,11 @@ namespace IksNativeClient.Common.Common
     /// </summary>
     public static class ScreenDef
     {
-        /// <summary>画面ID：友達一覧画面</summary>
-        public const short SCREEN_ID_FRIENDS = 0;
-        /// <summary>画面ID：チャットルーム画面</summary>
-        public const short SCREEN_ID_CHATROOM = 1;
-        /// <summary>画面ID：友達追加画面</summary>
-        public const short SCREEN_ID_FRIEND_ADD = 2;
+        /// <summary>機能ID：友達一覧機能</summary>
+        public const short FNC_ID_FRIENDS = 0;
+        /// <summary>機能ID：チャットルーム機能</summary>
+        public const short FNC_ID_CHATROOM = 1;
+        /// <summary>機能ID：友達追加機能</summary>
+        public const short FNC_ID_FRIEND_ADD = 2;
     }
 }
