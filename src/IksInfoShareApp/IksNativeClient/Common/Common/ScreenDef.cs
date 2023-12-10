a48aefaa0db76bd2238b953fea786f5d2189b0fa
“inakuuun” <bkg.inamoto@gmail.com>
2023-12-10 12:00:52 +0900

    ★NavMenuボタン処理対応 #11 - https://github.com/inakuuun/IksInfoShare/issues/11

--- src/IksInfoShareApp/IksNativeClient/Common/Common/ScreenDef.cs
+++ src/IksInfoShareApp/IksNativeClient/Common/Common/ScreenDef.cs
@@ -11,11 +11,11 @@ namespace IksNativeClient.Common.Common
     /// </summary>
     public static class ScreenDef
     {
-        /// <summary>機能ID：友達一覧機能</summary>
+        /// <summary>機能ID：友達一覧</summary>
         public const short FNC_ID_FRIENDS = 0;
-        /// <summary>機能ID：チャットルーム機能</summary>
+        /// <summary>機能ID：チャットルーム</summary>
         public const short FNC_ID_CHATROOM = 1;
-        /// <summary>機能ID：友達追加機能</summary>
+        /// <summary>機能ID：友達追加</summary>
         public const short FNC_ID_FRIEND_ADD = 2;
     }
 }
